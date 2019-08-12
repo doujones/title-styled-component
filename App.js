52377429b09fa09a43fd471dbedbf7b964dbc456
Dou Jones <misterchiefexecutiveofficer@yahoo.com>
2019-08-11 21:16:43 -0400

    second commit

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import  Superman1  from "./components/Superman"
 import './App.css';
 import image1 from "./components/images/img1.jpg"
+import image2 from "./components/images/img2.jpg"
+import image3 from "./components/images/img3.jpg"
 import Boswell from "./components/boswell"
 function App () {
   return(
@@ -10,6 +12,10 @@ function App () {
     <Boswell title="Hello Boswell"/>
     </Superman1>
     <superman1 boswell/>
+    <hr/>
+    <Superman1 img={image2}/>
+    <hr/>
+    <Superman1 img={image3}/>
     </div>
   )
 }
